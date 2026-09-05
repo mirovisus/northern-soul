c1db456dc0cb476efd940acbcba58e6d8f32ee55
mirovisus <119511410+mirovisus@users.noreply.github.com>
2026-09-05 18:36:44 +0200

    add loading spinner

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,9 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+requestAnimationFrame(function() {
+  requestAnimationFrame(function() {
+    window.dispatchEvent(new Event('react-mounted'));
+  });
+})
